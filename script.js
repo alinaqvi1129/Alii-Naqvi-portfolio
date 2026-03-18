09a97f5ac3aa32704eae4214e2af0110a8f135b4
Rishi <shakeelnaqvi35@gmail.com>
2026-03-18 15:33:00 +0530

    updated dark mode + UI improvements

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 var typed = new Typed(".typing", {
-
-strings:[
-"Frontend Developer",
-"Java Developer",
-"Machine Learning Learner"
-],
-
-typeSpeed:100,
-backSpeed:50,
-loop:true
-
+  strings:[
+    "Frontend Developer",
+    "Java Developer",
+    "Machine Learning Learner"
+  ],
+  typeSpeed:100,
+  backSpeed:50,
+  loop:true
 });
 
 
-// contact form
+// EmailJS initialization
+(function(){
+  emailjs.init("0S9gfAHhQCjx8pX6Ob2B");
+})();
 
-document.getElementById("contactForm").addEventListener("submit",async e=>{
 
-e.preventDefault()
+// Contact form
+document.getElementById("contact-form").addEventListener("submit", function(e){
+  e.preventDefault();
 
-const name=document.getElementById("name").value
-const email=document.getElementById("email").value
-const message=document.getElementById("message").value
+  emailjs.send("service_2uwd3l9","template_z3e3rpc", {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  })
+  .then(function(){
+    alert("Message sent successfully!");
+  })
+  .catch(function(error){
+    alert("Failed to send message");
+    console.log(error);
+  });
+});
 
-await fetch("http://localhost:5000/contact",{
 
-method:"POST",
+const toggle = document.getElementById("darkToggle");
 
-headers:{
-"Content-Type":"application/json"
-},
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark-mode");
+  toggle.checked = true;
+}
 
-body:JSON.stringify({name,email,message})
-
-})
-
-alert("Message sent!")
-
-})
+// Toggle change
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
